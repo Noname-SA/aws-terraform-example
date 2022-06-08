12691da748b6e491d287bc7beacd84853d95fb48
Michael Jones <25686778+mikeacjones@users.noreply.github.com>
2022-06-08 16:19:42 -0400

    Updated RHEL and AWS scripts

--- ec2-remote-engine/scripts/ec2_setup_AWS.sh
+++ ec2-remote-engine/scripts/ec2_setup_AWS.sh
@@ -5,6 +5,6 @@ sudo mkdir /opt/noname
 sudo wget -O nonamesecurity.tar.gz ${package_url}
 sudo tar -zxf nonamesecurity.tar.gz -C /opt/noname
 cd /opt/noname
-sudo ./install.sh --engine-with-mongo --management-ip ${noname_management_host} --collector-name ${remote_engine_name} --collector-urls <COLLECTOR_HOST1, COLLECTOR_HOST2, ...>
+sudo ./install.sh --engine-with-mongo --management-ip ${noname_management_host} --collector-name ${remote_engine_name} --collector-urls ${remote_engine_urls}
 sleep 10s
 sudo reboot

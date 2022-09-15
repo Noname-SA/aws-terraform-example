295cf15ec9630964404d281f447a58c93556f7ee
Michael Jones <25686778+mikeacjones@users.noreply.github.com>
2022-09-15 08:40:38 -0400

    Update ec2_setup_UBUNTU.sh

--- modules/noname-remote-engine/scripts/ec2_setup_UBUNTU.sh
+++ modules/noname-remote-engine/scripts/ec2_setup_UBUNTU.sh
@@ -5,6 +5,6 @@ sudo mkdir -p /opt/noname
 sudo wget -O nonamesecurity.tar.gz ${package_url}
 sudo tar -zxf nonamesecurity.tar.gz -C /opt/noname
 cd /opt/noname
-sudo ./install.sh --engine-with-mongo --management-ip ${noname_management_host} --collector-name ${remote_engine_name} --collector-urls ${remote_engine_urls} --noninteractive --force
+sudo ./noname_installer.sh --engine --management-hostname ${noname_management_host} --remote-engine-name ${remote_engine_name} --remote-engine-hostnames ${remote_engine_urls} --force
 sleep 10s
 sudo reboot
